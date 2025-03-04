4f166392a75ca691fa01698e51de14ad32be0d79
Claiko <jonathan.dellova42@gmail.com>
2025-03-04 11:59:46 +0100

    Initial project structure and necessary files.

---
+++ DX12RadianceCascades/src/D3D12RadianceCascades.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "DXSample.h"
+#include "StepTimer.h"
+#include "RaytracingHlslCompat.h"
+
+class D3D12RadianceCascades : public DXSample
+{
+public:
+    D3D12RadianceCascades(UINT width, UINT height, std::wstring name);
+
+    // IDeviceNotify
+    virtual void OnDeviceLost() override;
+    virtual void OnDeviceRestored() override;
+
+    // Messages
+    virtual void OnInit();
+    virtual void OnUpdate();
+    virtual void OnRender();
+    virtual void OnSizeChanged(UINT width, UINT height, bool minimized);
+    virtual void OnDestroy();
+    virtual IDXGISwapChain* GetSwapchain() { return m_deviceResources->GetSwapChain(); }
+
+private:
+};
---
+++ DX12RadianceCascades/src/Main.cpp
@@ -0,0 +1,8 @@
+#include "rcpch.h"
+#include "D3D12RadianceCascades.h"
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int nCmdShow)
+{
+	D3D12RadianceCascades radianceCascadesSample(1280, 720, L"D3D12 Radiance Cascades - Master Thesis Project");
+	return Win32Application::Run(&radianceCascadesSample, hInstance, nCmdShow);
+}
