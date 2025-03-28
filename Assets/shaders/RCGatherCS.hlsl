37e6ff58ec726445058755a66ad9b13f139a84e1
Claiko <jonathan.dellova42@gmail.com>
2025-03-28 10:31:01 +0100

    refactor: Moved probe info creation to RCCommon.

--- Assets/shaders/RCGatherCS.hlsl
+++ Assets/shaders/RCGatherCS.hlsl
@@ -13,7 +13,7 @@ float4 RayMarch(float2 origin, float2 direction, float range, float2 texelSize)
     for (int i = 0; i < 100; i++)
     {
         float2 samplingPoint = origin + direction * min(distanceTraveled, range) * texelSize;
-        if (IS_OUT_OF_BOUNDS_RELATIVE(samplingPoint))
+        if (OUT_OF_BOUNDS_RELATIVE(samplingPoint))
         {
             return float4(0.0f, 0.0f, 0.0f, TRANSPARENT);
         }
@@ -34,39 +34,15 @@ float4 RayMarch(float2 origin, float2 direction, float range, float2 texelSize)
     return float4(0.0f, 0.0f, 0.0f, TRANSPARENT);
 }
 
-struct ProbeInfo
-{
-    float rayCount;
-    uint2 probeIndex;
-    float2 probeSpacing;
-    float rayIndex;
-    float startDistance;
-    float range;
-    float2 texelSize;
-};
-
 [numthreads(16, 16, 1)]
 void main(uint3 DTid : SV_DispatchThreadID)
 {
     uint2 pixelPos = DTid.xy;
-    
-    uint probesPerDim = THIS_PROBES_PER_DIM(cascadeInfo.cascadeIndex);
-    float2 relative = (float2) pixelPos / (cascadeInfo.probePixelSize * probesPerDim);
+    float2 relative = float2(pixelPos) / CASCADE_SIDE_LENGTH(cascadeInfo.cascadeIndex);
 
-    if (!IS_OUT_OF_BOUNDS_RELATIVE(relative))
+    if (!OUT_OF_BOUNDS_RELATIVE(relative))
     {
-        ProbeInfo probeInfo;
-        
-        probeInfo.rayCount = THIS_RAYS_PER_PROBE(cascadeInfo.cascadeIndex);
-        probeInfo.probeIndex = floor(pixelPos / cascadeInfo.probePixelSize);
-        probeInfo.probeSpacing = globals.probeSpacing0 * pow(globals.probeScalingFactor, cascadeInfo.cascadeIndex);
-        uint2 rayIndex2D = pixelPos % cascadeInfo.probePixelSize;
-        probeInfo.rayIndex = rayIndex2D.x + rayIndex2D.y * cascadeInfo.probePixelSize;
-        probeInfo.startDistance = sign(cascadeInfo.cascadeIndex) * GeometricSeriesSum(globals.rayLength0, globals.rayScalingFactor, cascadeInfo.cascadeIndex);
-        probeInfo.range = globals.rayLength0 * pow(globals.rayScalingFactor, cascadeInfo.cascadeIndex);
-        float d = globals.probeSpacing0 * pow(2.0, cascadeInfo.cascadeIndex + 1);
-        probeInfo.range += sign(cascadeInfo.cascadeIndex) * length(float2(d, d));
-        probeInfo.texelSize = 1.0f / (probesPerDim * cascadeInfo.probePixelSize);
+        ProbeInfo probeInfo = BuildProbeInfo(pixelPos, cascadeInfo.cascadeIndex);
         
         {
             float2 probeOrigin = (float2(probeInfo.probeIndex) + 0.5f) * probeInfo.probeSpacing * probeInfo.texelSize;
